18fe93630fd31b4706a891237ce1389626dc1be1
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-04-23 11:26:47 -0300

    hook de permissão

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,17 @@
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { useCan } from '../hooks/useCan';
 import { setupApiClient } from '../services/api';
 import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { isAuthenticated, user } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  });
+
   useEffect(() => {
     api
       .get('/me')
@@ -14,9 +20,10 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   );
 }
 
